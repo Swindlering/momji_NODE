e5149af02ee212ef8e7321df203852c165b0f538
Guidi Tognon <tguidi@inbox.fr>
2021-07-08 23:25:46 +0200

    chore: npm init

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+// Provides Express middleware to enable CORS with various options.
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());  /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+
+const db = require("./app/models");
+
+db.sequelize.sync();
+// Do not un comment for persistance data
+// drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "MOMJI API." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
